c9a910cfef2f31c74e4560c686064d396884c62d
mahir-shaikh <mahirthebest95@gmail.com>
2021-05-09 21:55:59 +0530

    Added express as a requirement for heroku initialization

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 
 const dotenv = require('dotenv')
 dotenv.config()
+
+const PORT = process.env.PORT || 3000;
+const express = require('express');
+
 const logger = require('./libs/logger')
 const dbConnection = require('./libs/db-connection')
 const sbConnection = require('./libs/slack-connection')
 
+//Initialize App
+const app = express();
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+})
 // Connection to MongoDB
 dbConnection.connect();
 dbConnection.startCron();
