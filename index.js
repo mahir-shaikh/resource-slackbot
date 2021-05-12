53e478a478fa27148db7ebfdd93858b916db6f94
mahir-shaikh <mahirthebest95@gmail.com>
2021-05-12 13:25:58 +0530

    Removed Express as we are using Heroku Worker instead of Heroku Web.

--- index.js
+++ index.js
@@ -2,56 +2,13 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
-const PORT = process.env.PORT || 3000;
-const express = require('express');
-
 const logger = require('./libs/logger')
 const dbConnection = require('./libs/db-connection')
 const sbConnection = require('./libs/slack-connection')
 
-//Initialize App
-const app = express();
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`)
-})
-
-app.get('/', (req, res) => {
-    console.log('Someone tried to access via URL')
-    res.send(
-        "<h1 style='text-align:center'>Things are looking good!!!</h1>"
-    )
-})
 // Connection to MongoDB
 dbConnection.connect();
 dbConnection.startCron();
 //
 sbConnection.createBot()
 sbConnection.attachListeners()
-
-
-//Keep app alive as Heroku sleeps in 30mins of inactivity
-var http = require('http'); //importing http
-
-function startKeepAlive() {
-    setInterval(function() {
-        var options = {
-            host: 'appdemo-usage-bot.herokuapp.com',
-            path: '/'
-        };
-        http.get(options, function(res) {
-            res.on('data', function(chunk) {
-                try {
-                    // optional logging... disable after it's working
-                    console.log("HEROKU RESPONSE: " + chunk);
-                } catch (err) {
-                    console.log(err.message);
-                }
-            });
-        }).on('error', function(err) {
-            console.log("Error: " + err.message);
-        });
-    }, 20 * 60 * 1000); // load every 20 minutes
-}
-if(process.env.KEEP_ALIVE){
-    startKeepAlive();
-}
